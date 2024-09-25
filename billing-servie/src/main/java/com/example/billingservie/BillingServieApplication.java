cb9ffea1d9c9ec22f399cd25b474970fc7ebff82
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-09-25 13:14:01 +0100

    add billing service

---
+++ billing-servie/src/main/java/com/example/billingservie/BillingServieApplication.java
@@ -0,0 +1,13 @@
+package com.example.billingservie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingServieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillingServieApplication.class, args);
+    }
+
+}
---
+++ billing-servie/src/test/java/com/example/billingservie/BillingServieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.billingservie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
