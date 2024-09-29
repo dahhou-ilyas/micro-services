cfba5a8b196d40efb7b960c915db81162eb00622
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-09-29 23:59:10 +0100

    create jwt

--- auth-service/src/main/java/com/example/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/example/authservice/service/AuthService.java
@@ -3,6 +3,7 @@ package com.example.authservice.service;
 import com.example.authservice.entities.UserCredentiel;
 import com.example.authservice.repository.UserCredentielRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,10 +12,16 @@ public class AuthService {
     @Autowired
     private UserCredentielRepository userCredentielRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
     public String saveUser(UserCredentiel userCredentiel) {
+        userCredentiel.setPassword(passwordEncoder.encode(userCredentiel.getPassword()));
         userCredentielRepository.save(userCredentiel);
         return userCredentiel.getUsername();
     }
+
 
 
 }
---
+++ auth-service/src/main/java/com/example/authservice/service/JwtService.java
@@ -0,0 +1,46 @@
+package com.example.authservice.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+    public static final String SECRET = "DJD586TYG6TIBTVITI688TOB8YBO7T6R5RC5C4C4X5XSDCE65V";
+
+    public void validateToken(String token) {
+        Jws<Claims> claimsJws = Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+
+
+    }
+
+    public String generateToken(String userName) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims,userName);
+    }
+
+    private String createToken(Map<String, Object> claims, String userName) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userName)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+100*60*30))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
