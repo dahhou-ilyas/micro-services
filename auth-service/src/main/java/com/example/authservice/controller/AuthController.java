7f168dc6c75adf91ca69dd44718cf59bebb87683
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-09-30 22:32:38 +0100

    add more func

---
+++ auth-service/src/main/java/com/example/authservice/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.example.authservice.controller;
+
+import com.example.authservice.entities.UserCredentiel;
+import com.example.authservice.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public String addUser(@RequestBody UserCredentiel userCredentiel) {
+        return authService.saveUser(userCredentiel);
+    }
+
+    @PostMapping("/token")
+    public String getToken(UserCredentiel userCredentiel) {
+        return authService.generateToken(userCredentiel.getUsername());
+    }
+
+    @GetMapping("/validate")
+    public String validateToken(@RequestParam("token") String token) {
+        authService.validateToken(token);
+        return "valid";
+    }
+
+}
